a320264e30b55e1ec2562cdd62e932e041924cb5
Douglas Baltazar <douglasbaltazar1@gmail.com>
2021-05-17 13:11:18 -0300

    Autentificação

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -10,9 +10,18 @@ import Button from '../../components/button'
 import logoImg from '../../assets/logo.svg'
 import getValidationErrors from '../../utils/getValidationErrors';
 import { FormHandles } from '@unform/core';
+import { useAuth } from '../../context/AuthContext';
+
+interface SignInFormData {
+    email: string;
+    password: string;
+}
 
 const SignIn: React.FC = () => {
-    const handleSubmit = useCallback(async (data: object) => {
+    const formRef = useRef<FormHandles>(null)
+    const { user, signIn } = useAuth() 
+    console.log(user)
+    const handleSubmit = useCallback(async (data: SignInFormData) => {
         formRef.current?.setErrors({});
         console.log(data);
         try {
@@ -23,13 +32,18 @@ const SignIn: React.FC = () => {
             await schema.validate(data, {
                 abortEarly: false,
             });
+            signIn({
+                email: data.email,
+                password: data.password,
+            });
         } catch(err) {
             const errors = getValidationErrors(err);
             formRef.current?.setErrors(errors)
         }
 
-    }, []);
-    const formRef = useRef<FormHandles>(null)
+    }, [signIn]);
+
+    
     return (
     <Container>
         <Content>
